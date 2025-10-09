44ee9c586132cbb59b061068f444332b43775319
Dipayan Das <d.dipayan2002@gmail.com>
2025-10-10 02:42:52 +0530

    Added about us bento box

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/Navbar";
+import BentoShowcase from "@/components/home/BentoShowcase";
 
 export default function HomePage() {
   return (
     <main className="relative min-h-screen  overflow-hidden">
       <Navbar />
       <Hero />
+      <BentoShowcase />
     </main>
   );
 }
