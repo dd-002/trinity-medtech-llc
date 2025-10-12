e0cf5001f00f2856c516a2b80680614a747ee8df
Dipayan Das <d.dipayan2002@gmail.com>
2025-10-13 01:07:53 +0530

    added splashscreen

---
+++ src/components/home/SplashScreen.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+export default function SplashScreen() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isExiting, setIsExiting] = useState(false);
+
+  useEffect(() => {
+    // Start exit after 2.5s
+    const timer = setTimeout(() => {
+      setIsExiting(true);
+      // Remove after animation ends (0.8s)
+      setTimeout(() => setIsVisible(false), 800);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) return null;
+
+  return (
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center bg-black  transition-transform duration-700 ease-in-out
+      ${isExiting ? "-translate-y-full" : "translate-y-0"}`}
+       style={{ zIndex: 9999 }}
+    >
+      <div className="animate-fade-in-up">
+        <Image
+          src="/logos/logoc.svg"
+          alt="Splash Logo"
+          width={200}
+          height={200}
+          className="animate-pulse"
+        />
+      </div>
+    </div>
+  );
+}
